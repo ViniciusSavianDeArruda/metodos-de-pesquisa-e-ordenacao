c5dd11e96fe5cf58612e74ad60a3bc73cdf0c692
Vinicius Arruda <arrudavinicius283@gmail.com>
2026-08-21 14:19:43 -0300

    refactor(python): remove estrutura antiga do MVC

--- 01-IdeiaMVC/Python/Principal.py
+++
@@ -1,6 +0,0 @@
-# Importa o controller a partir da pasta dele
-from controller.controller import MedicaoTempoController
-
-if __name__ == "__main__":
-    programa = MedicaoTempoController()
-    programa.executar()
